f3c210659889c73648149c4d2b028ba140d54805
= <Pushks2015@gmail.com>
2022-10-30 23:59:39 +0530

    added audio player

--- src/Player/audios.js
+++ src/Player/audios.js
@@ -10,5 +10,14 @@ exports.songsdata = [
     {
         "title": "Travis Scott - Stop trina be God",
         "url": "https://beardbarnmusicbucket.s3.amazonaws.com/The+Wild+Horse"
+    },
+    {
+        'title': 'Notorious BIG: Who shot ya',
+        'url': 'https://www.youtube.com/watch?v=2xnqp6CPM8o'
+    },
+    {
+        'title': 'Six Days',
+        'src': './assets/SixDays(Remix).mp3'
     }
+
 ]
